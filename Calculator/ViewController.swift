2740a4a22843ae120a078c1c30b34b38c79b1c93
Vincent Guo <vincent.weiguo@gmail.com>
2015-02-03 00:36:36 -0800

    Lecture 1: Logistics, iOS 8 Overview

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypeingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        if userIsInTheMiddleOfTypeingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypeingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
